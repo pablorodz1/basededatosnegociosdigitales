5c93d43993172bc824ac850e5882108b78bfba86
pablorodz1 <rickyxsan214@gmail.com>
2024-01-18 15:53:41 -0600

    practica2 terminada con 5 tablas

--- UNIDAD 1/Pracrica2.sql
+++ UNIDAD 1/Pracrica2.sql
@@ -4,7 +4,7 @@ create database practica2
 go
 use practica2
 -- Crear tabla cliente
-CREATE TABLE tblcliente (
+CREATE TABLE tbl_cliente (
     idcliente INT not null,
     nombre VARCHAR(100) UNIQUE,
     direccion VARCHAR(100),
@@ -39,3 +39,28 @@ CREATE TABLE tblventa (
     FOREIGN KEY (idcliente) REFERENCES tblcliente(idcliente),
     FOREIGN KEY (idempleado) REFERENCES tblempleado(idempleado)
 );
+
+CREATE TABLE tbldetalleventa (
+idventa int not null,
+idempleado int not null,
+cantidad int not null,
+precio decimal (10,2) not null,
+constraint pk_detallesventa
+primary key (idventa, idempleado),
+foreign key (idventa) references tblventa(idventa),
+foreign key (idempleado) references tblempleado(idempleado)
+);
+
+CREATE TABLE tbl_producto (
+    idproducto INT NOT NULL,
+    descripcion VARCHAR(200) NOT NULL,
+    existencia INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    idventa INT NOT NULL,
+    idempleado INT NOT NULL,
+    CONSTRAINT pk_producto PRIMARY KEY (idproducto),
+    CONSTRAINT fk_venta FOREIGN KEY (idventa) REFERENCES tblventa(idventa),
+    CONSTRAINT fk_empleado FOREIGN KEY (idempleado) REFERENCES tblempleado(idempleado)
+);
+
+
