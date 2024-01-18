0943e01abce52e3550039880555c9b3bf8d0f152
pablorodz1 <rickyxsan214@gmail.com>
2024-01-18 17:55:20 -0600

    practica#1 terminada

--- UNIDAD 1/Ejercicio1.sql
+++ UNIDAD 1/Ejercicio1.sql
@@ -1,24 +1,40 @@
--- SQL -LDD	
-
--- Crear la base de datos bdentornosvip
+-- SQL-LDD
+-- crear la base de datos bdentornosvip
 
 create database bdentornosvip
+
+go
+-- cambair de base de datos 
+use bdentornosvip
 go 
 
--- cambiar de base de datos
-
-USE bdentornosvip
-go
-
--- crear tabla categoria
-
-create table tblcategoria (
-	categoriaid int not null,
-	descripcion varchar(100) not null,
-	constraint pk_tblcategoria
-	primary key(categoriaid),
-	constraint unique_descripcion
-	unique(descripcion)
-
+-- crea una tabla categoria 
+create table tblcategoria(
+categoriaId int not null,
+descripcion varchar(100) not null,
+constraint pk_tblcategoria
+primary key (categoriaId),
+unique (descripcion)
 )
+go 
+
+-- crear la tabla productos 
+CREATE TABLE tblProducto(
+  productoId int not null,
+  nombreP varchar(50) not null,
+  precio decimal(10,2) not null,
+  existencia int not null,
+  categoriaId int not null,
+  constraint pk_tblProducto
+  primary key(productoId),
+  constraint unique_nombreP
+  unique(nombreP),
+  constraint chk_precio
+  check(precio>0.0 and precio<=10000),
+  constraint chk_existencia
+  check(existencia>=0),
+  constraint fk_tblProducto_tblcategoria
+  foreign key(categoriaId)
+  references tblcategoria(categoriaId)
+);
 go
